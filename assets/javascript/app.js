8c1ffb500f00e790db338f00b728a773f712bc5e
L-Ward <noreply>
2018-07-14 10:16:04 -0700

    added firebase and fleshed out html

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCcs5MKqu2_JZz95K1nGtYDLiOFnaw-glo",
+    authDomain: "leahsfirstfirebaseproject.firebaseapp.com",
+    databaseURL: "https://leahsfirstfirebaseproject.firebaseio.com",
+    projectId: "leahsfirstfirebaseproject",
+    storageBucket: "leahsfirstfirebaseproject.appspot.com",
+    messagingSenderId: "559743623378"
+  };
+  firebase.initializeApp(config);
