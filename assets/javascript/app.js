c2f68ebead2fb5ce928eef1e58c10e8a9d81624a
L-Ward <noreply>
2018-07-14 11:39:46 -0700

    functionality to add new train to firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,3 +10,38 @@ var config = {
   firebase.initializeApp(config);
 
   var database = firebase.database();
+
+  // Adding employee infor on click of submit button
+  $("#train-submit").on("click", function() {
+      event.preventDefault();
+
+    //get train info from form
+    var trainName = $("#train-name-input").val().trim();
+    var trainDestination = $("#train-destination-input").val().trim();
+    var firstTrain = $("#first-train-input").val().trim();
+    var trainFrequency = $("#train-frequency-input").val().trim();
+
+    // Temporary object to hold train info
+    var newTrain = {
+        name: trainName,
+        destination: trainDestination,
+        firstTime: firstTrain,
+        frequency: trainFrequency
+    };
+
+    //Add tain info to firebase database
+    database.ref().push(newTrain);
+
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.firstTime);
+    console.log(newTrain.frequency);
+
+    //clear input form
+    $("#train-name-input").val("");
+    $("#train-destination-input").val("");
+    $("#first-train-input").val("");
+    $("#train-frequency-input").val("");
+  });
+
+  
